8b055ec4a2bda4b9a5728f6cf0cdad3a37e25fcf
sultana2020 <s2020sana@gmail.com>
2020-07-15 13:22:13 -0400

    Class Exercise Repo

---
+++ GidExercise/src/main/java/GidExercise/GidExercise/App.java
@@ -0,0 +1,13 @@
+package GidExercise.GidExercise;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
